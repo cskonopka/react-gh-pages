b302fe9dacc8368c47d069c2a038a4b79d840508
Christopher Konopka <cskonopka@schwwaaa.net>
2018-03-27 01:07:29 -0400

    updated build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Example from './Example';
 import logo2 from './csk-logo-transparent.png'
@@ -9,31 +8,26 @@ class App extends Component {
   render() {
     return (
       <div>
-      <div className="heads">
-      <Grid container columns={1} >
-        <Grid.Column>
-          <Image src={logo2} href='http://cskonopka.com' size='medium' centered/>
-        </Grid.Column>
-      </Grid>
-      <Grid container columns={1} >
-        <Grid.Column>
-          <Header  size='huge'>VIDEO TITLE SEARCH</Header>
-        </Grid.Column>
-      </Grid>
-      <Grid container columns={1} >
-        <Grid.Column>
-          <Example/>
-        </Grid.Column>
-      </Grid>
-
-        </div>
+          <div className="heads">
+              <Grid container columns={1}>
+                  <Grid.Column>
+                      <Image src={logo2} href='http://cskonopka.com' size='medium' centered/>
+                  </Grid.Column>
+              </Grid>
+              <Grid container columns={1}>
+                  <Grid.Column>
+                      <Header size='huge'>VIDEO TITLE SEARCH</Header>
+                  </Grid.Column>
+              </Grid>
+              <Grid container columns={1}>
+                  <Grid.Column>
+                      <Example/>
+                  </Grid.Column>
+              </Grid>
+          </div>
       </div>
     );
   }
 }
 
 export default App;
-
-        // <div className="heads">
-
-        // </div>
--- src/Example.js
+++ src/Example.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Autosuggest          from 'react-autosuggest';
-import { Container }        from 'semantic-ui-react';
 import testJSON             from './cskonopka-vaultslim.json'
 
 const getSuggestions = value => {
@@ -24,11 +23,6 @@ export default class Example extends Component  {
   constructor() {
     super();
 
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
     this.state = {
       value: '',
       suggestions: []
@@ -41,15 +35,12 @@ export default class Example extends Component  {
     });
   };
 
-  // Autosuggest will call this function every time you need to update suggestions.
-  // You already implemented this logic above, so just use it.
   onSuggestionsFetchRequested = ({ value }) => {
     this.setState({
       suggestions: getSuggestions(value)
     });
   };
 
-  // Autosuggest will call this function every time you need to clear suggestions.
   onSuggestionsClearRequested = () => {
     this.setState({
       suggestions: []
@@ -59,14 +50,12 @@ export default class Example extends Component  {
   render() {
     const { value, suggestions } = this.state;
 
-    // Autosuggest will pass through all these props to the input.
     const inputProps = {
       placeholder: 'type a letter, find a title',
       value,
       onChange: this.onChange
     };
 
-    // Finally, render it!
     return (
       <Autosuggest
         suggestions={suggestions}
